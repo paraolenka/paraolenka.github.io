815aa08cf2e34638caa9d128464c6736b8b51a38
paraolenka <164431368+paraolenka@users.noreply.github.com>
2024-03-21 18:55:27 -0500

    Add files via upload

---
+++ java.js
@@ -0,0 +1,8 @@
+onload = () => {
+    const c = setTimeout(() => {
+      document.body.classList.remove("not-loaded");
+      clearTimeout(c);
+    }, 1000);
+  };
+  window.alert("¡Hola Olenka! Feliz día. Quiero disculparme por no haberte regalado flores amarillas, pero en su lugar, te he programado unas hermosas flores azules. Fue un desafío, requirió muchas líneas de código, pero espero que te guste igualmente.");
+  alert("¡Hola Olenka! Feliz día. Quiero disculparme por no haberte regalado flores amarillas, pero en su lugar, te he programado unas hermosas flores azules. Fue un desafío, requirió muchas líneas de código, pero espero que te guste igualmente.");
